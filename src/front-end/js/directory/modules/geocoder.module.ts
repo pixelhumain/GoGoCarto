816ff80db7d51f79009d67635541bac32451afa1
Sebastian Castro <90scastro@gmail.com>
2017-01-06 17:22:23 +0100

    Change google geocoder to geocoder-js

--- src/front-end/js/directory/modules/geocoder.module.ts
+++ src/front-end/js/directory/modules/geocoder.module.ts
@@ -1,25 +1,44 @@
-declare let google;
+declare let GeocoderJS;
+declare let App : AppModule;
+declare var L;
 
+import { AppModule } from "../app.module";
 import { Event, IEvent } from "../utils/event";
+
+export class GeocodeResult
+{
+
+}
 
 export class GeocoderModule
 {
 	onResult = new Event<any>();
+	geocoder : any = null;
+	geocoderOSM : any = null;
+	
+	constructor()
+	{
+		this.geocoder = GeocoderJS.createGeocoder('openstreetmap');
+		//this.geocoder = GeocoderJS.createGeocoder({'provider': 'google'});
+	}
 
 	geocodeAddress( address, callbackComplete?, callbackFail? ) {
 
 		console.log("geocode address : ", address);
-		let geocoder = new google.maps.Geocoder();
 
-		geocoder.geocode( { 'address': address}, (results, status) =>
-		{
-			if (status == google.maps.GeocoderStatus.OK) 
+		this.geocoder.geocode( address, (results) =>
+		{			
+			if (results !== null) 
 			{
-				if (callbackComplete) callbackComplete(results);
-				console.log("geocode result", results);
-
-				let zoom = this.calculateAppropriateZoomForResults(results[0]);
-				this.onResult.emit({location : results[0].geometry.location, zoom : zoom});
+				if (callbackComplete) callbackComplete(results[0]);	
+				
+				var corner1 = L.latLng(results[0].bounds[0], results[0].bounds[1]),
+				corner2 = L.latLng(results[0].bounds[2], results[0].bounds[3]),
+				bounds = L.latLngBounds(corner1, corner2);
+				
+				setTimeout( () => { console.log("fitbounds OSM", bounds); App.map().fitBounds(bounds);}, 500);
+				
+				//this.onResult.emit(results[0]);
 			} 	
 			else
 			{
@@ -28,9 +47,5 @@ export class GeocoderModule
 		});
 	};
 
-	calculateAppropriateZoomForResults( result) {
-		// TODO
-		return 12;
-	};
 
 }
