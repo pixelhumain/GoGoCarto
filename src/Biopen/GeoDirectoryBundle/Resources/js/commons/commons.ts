b9ffdcd5d9abcd08cb6959ee2f24f3e4b6dcbee0
Sebastian Castro <90scastro@gmail.com>
2017-01-20 10:31:58 +0100

    Remove lowerCase in slugify

--- src/Biopen/GeoDirectoryBundle/Resources/js/commons/commons.ts
+++ src/Biopen/GeoDirectoryBundle/Resources/js/commons/commons.ts
@@ -18,7 +18,7 @@ export function redirectToDirectory(route, address = $('#search-bar').val(), ran
 export function slugify(text) : string
 {
   if (!text) return '';
-  return text.toString().toLowerCase()
+  return text.toString()//.toLowerCase()
     .replace(/\s+/g, '-')           // Replace spaces with -
     .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
     .replace(/\-\-+/g, '-')         // Replace multiple - with single -
