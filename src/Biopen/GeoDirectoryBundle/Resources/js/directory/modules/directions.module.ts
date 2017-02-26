498a19bb92ff9a32e394014489993139a8998cdb
Sebastian Castro <90scastro@gmail.com>
2017-02-26 18:11:54 +0100

    Preparing directory-menu for larger activities

--- src/Biopen/GeoDirectoryBundle/Resources/js/directory/modules/directions.module.ts
+++ src/Biopen/GeoDirectoryBundle/Resources/js/directory/modules/directions.module.ts
@@ -14,7 +14,8 @@ export class DirectionsModule
 
   constructor() {
   	window.lrmConfig = {
-			serviceUrl: '//router.project-osrm.org/viaroute',
+			// TODO change this demo serviceUrl
+			// 		serviceUrl: '//router.project-osrm.org/viaroute',
 			//    profile: 'mapbox/driving',
 		};
 
@@ -53,63 +54,17 @@ export class DirectionsModule
 					routeWhileDragging: false
 				}
 			),
+			langage: 'fr',
 			routeWhileDragging: false,
 			showAlternatives: false,
 			altLineOptions: {
 				styles: [
 					{color: 'black', opacity: 0.15, weight: 9},
 					{color: 'white', opacity: 0.8, weight: 6},
-					{color: 'red', opacity: 0.5, weight: 2}
+					{color: '#00b3fd', opacity: 0.5, weight: 2}
 				]
 			}
 		}).setPosition('bottomleft').addTo(App.map());			
-
-  	// this.directionsService_.route({
-   //  	origin: origin,
-   //  	destination: destination,
-   //  	travelMode: google.maps.TravelMode.DRIVING
-  	// }, (response, status) => {
-	  //   if (status === google.maps.DirectionsStatus.OK) 
-	  //   {
-	  //     	google.maps.event.trigger(App.map(), 'resize');
-	  //     	this.directionsRenderer_.setDirections(response);		      	
-
-			// let distance_to_reach = response.routes[0].legs[0].distance.value / 2;
-			// let distance_somme = 0;
-			// let i = 0;
-			// let route = response.routes[0].legs[0];
-
-			// while(i < (route.steps.length - 1) && distance_somme < distance_to_reach)
-			// {
-			// 	i++;
-			// 	distance_somme += route.steps[i].distance.value;				
-			// }
-			
-			// let middleStep = Math.max(i,0);			
-			// this.clearDirectionMarker();
-
-			// let marker_position = route.steps[middleStep].path[Math.floor(route.steps[middleStep].path.length/2)];
-
-			// this.markerDirectionResult = new RichMarker({		
-			// 	map: App.map(),
-			// 	draggable: false,
-			// 	position: marker_position,
-			// 	flat: true
-			// }, null);
-
-			// let content = document.createElement("div");
-			// $(content).attr('id',"markerDirectionResult");
-			// $(content).addClass('arrow_box');
-			// let innerHtml = '<div class="duration">' + route.duration.text + "</div>";
-			// innerHtml    += '<div class="distance">' + route.distance.text + "</div>";
-			// content.innerHTML = innerHtml;
-			// this.markerDirectionResult.setContent(content);
-	    // } 
-	    // else
-	    // {
-	    //   $('#modal-directions-fail').openModal();
-	    // }
-  	// });
 	};
 
 	hideItineraryPanel()
