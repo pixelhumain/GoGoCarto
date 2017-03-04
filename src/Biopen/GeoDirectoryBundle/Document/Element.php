cceb1d0ade7f8a9b4765cbb7ad35adf75b1d0a21
Sebastian Castro <90scastro@gmail.com>
2017-03-04 21:14:24 +0100

    Fixs last mongoDbMigration issues

--- src/Biopen/GeoDirectoryBundle/Document/Element.php
+++ src/Biopen/GeoDirectoryBundle/Document/Element.php
@@ -7,7 +7,7 @@
  *
  * @copyright Copyright (c) 2016 Sebastian Castro - 90scastro@gmail.com
  * @license    MIT License
- * @Last Modified time: 2017-03-04 15:49:54
+ * @Last Modified time: 2017-03-04 20:55:26
  */
  
 
@@ -25,7 +25,7 @@ class Element
     /**
      * @var int
      *
-     * @MongoDB\Id(strategy="auto")
+     * @MongoDB\Id(strategy="ALNUM") 
      */
     private $id;
 
@@ -83,45 +83,40 @@ class Element
      *
      * @MongoDB\Field(type="string")
      */
-    private $webSite;
-
-    /**
-    * @MongoDB\ReferenceMany(targetDocument="Biopen\GeoDirectoryBundle\Document\ElementProduct", cascade={"persist", "remove"})
-    */
-    private $products; 
+    private $webSite;    
 
     /**
      * @var string
      *
-     * MongoDB\Field(type="string")
+     * @MongoDB\Field(type="string")
      */
     private $mainProduct;
 
-    /**
+   /**
      * @var string
      *
-     * MongoDB\Field(type="string")
+     * @MongoDB\Field(type="string")
      */
     private $type;
 
     /**
      * @var string
      *
-     * MongoDB\Field(type="string")
+     * @MongoDB\Field(type="string")
      */
     private $contributor;
 
-    /**
+   /**
      * @var string
      *
-     * MongoDB\Field(type="string")
+     * @MongoDB\Field(type="string")
      */
     private $contributorMail;
 
-    /**
+   /**
      * @var string
      *
-     * MongoDB\Field(type="string",)
+     * @MongoDB\Field(type="string")
      */
     private $validationCode;
 
@@ -132,25 +127,23 @@ class Element
      */
     private $valide = false;
 
+    /**
+    * @MongoDB\ReferenceMany(targetDocument="Biopen\GeoDirectoryBundle\Document\ElementProduct", cascade={"persist", "remove"})
+    */
+    private $products; 
 
     private $distance;
 
     private $wastedDistance;
 
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->products = new \Doctrine\Common\Collections\ArrayCollection();
-        $this->validationCode = md5(uniqid(rand(), true));
-        $this->contributor = '';
-    }
-
     public function reinitContributor()
     {
         $this->validationCode = md5(uniqid(rand(), true));
         $this->contributorMail = '';
+        $this->contributor = '';
+
+        // constructor
+        $this->validationCode = md5(uniqid(rand(), true));
         $this->contributor = '';
     }
 
@@ -167,13 +160,15 @@ class Element
         $waste = -1.0*count($this->getProducts())/10.0 + 1.0;
         return $this->getDistance() * $waste;
     }
-
- 
-
+    public function __construct()
+    {
+        $this->products = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
     /**
      * Get id
      *
-     * @return integer
+     * @return int_id $id
      */
     public function getId()
     {
@@ -184,20 +179,18 @@ class Element
      * Set name
      *
      * @param string $name
-     *
-     * @return Element
+     * @return $this
      */
     public function setName($name)
     {
         $this->name = $name;
-
         return $this;
     }
 
     /**
      * Get name
      *
-     * @return string
+     * @return string $name
      */
     public function getName()
     {
@@ -205,333 +198,9 @@ class Element
     }
 
     /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return Element
-     */
-    public function setDistance($distance)
-    {
-        $this->distance = $distance;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getDistance()
-    {
-        return $this->distance;
-    }
-
-
-    /**
-     * Set adresse
-     *
-     * @param string $adresse
-     *
-     * @return Element
-     */
-    public function setAdresse($adresse)
-    {
-        $this->adresse = $adresse;
-
-        return $this;
-    }
-
-    /**
-     * Get adresse
-     *
-     * @return string
-     */
-    public function getAdresse()
-    {
-        return $this->adresse;
-    }
-
-    /**
-     * Set description
-     *
-     * @param string $description
-     *
-     * @return Element
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    /**
-     * Get description
-     *
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    /**
-     * Set tel
-     *
-     * @param string $tel
-     *
-     * @return Element
-     */
-    public function setTel($tel)
-    {
-        $this->tel = $tel;
-
-        return $this;
-    }
-
-    /**
-     * Get tel
-     *
-     * @return string
-     */
-    public function getTel()
-    {
-        return $this->tel;
-    }
-
-    /**
-     * Set mail
-     *
-     * @param string $mail
-     *
-     * @return Element
-     */
-    public function setMail($mail)
-    {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    /**
-     * Get mail
-     *
-     * @return string
-     */
-    public function getMail()
-    {
-        return $this->mail;
-    }
-
-    /**
-     * Set webSite
-     *
-     * @param string $webSite
-     *
-     * @return Element
-     */
-    public function setWebSite($webSite)
-    {
-        $this->webSite = $webSite;
-
-        return $this;
-    }
-
-    /**
-     * Get webSite
-     *
-     * @return string
-     */
-    public function getWebSite()
-    {
-        return $this->webSite;
-    }
-
-    /**
-     * Set mainProduct
-     *
-     * @param string $mainProduct
-     *
-     * @return Element
-     */
-    public function setMainProduct($mainProduct)
-    {
-        $this->mainProduct = $mainProduct;
-
-        return $this;
-    }
-
-    /**
-     * Get mainProduct
-     *
-     * @return string
-     */
-    public function getMainProduct()
-    {
-        return $this->mainProduct;
-    }
-
-    /**
-     * Set type
-     *
-     * @param string $type
-     *
-     * @return Element
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-
-        return $this;
-    }
-
-    /**
-     * Get type
-     *
-     * @return string
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * Set contributor
-     *
-     * @param string $contributor
-     *
-     * @return Element
-     */
-    public function setContributor($contributor)
-    {
-        $this->contributor = $contributor;
-
-        return $this;
-    }
-
-    /**
-     * Get contributor
-     *
-     * @return string
-     */
-    public function getContributor()
-    {
-        return $this->contributor;
-    }
-
-    /**
-     * Set contributorMail
-     *
-     * @param string $contributorMail
-     *
-     * @return Element
-     */
-    public function setContributorMail($contributorMail)
-    {
-        $this->contributorMail = $contributorMail;
-
-        return $this;
-    }
-
-    /**
-     * Get contributorMail
-     *
-     * @return string
-     */
-    public function getContributorMail()
-    {
-        return $this->contributorMail;
-    }
-
-    /**
-     * Set validationCode
-     *
-     * @param string $validationCode
-     *
-     * @return Element
-     */
-    public function setValidationCode($validationCode)
-    {
-        $this->validationCode = $validationCode;
-
-        return $this;
-    }
-
-    /**
-     * Get validationCode
-     *
-     * @return string
-     */
-    public function getValidationCode()
-    {
-        return $this->validationCode;
-    }
-
-    /**
-     * Set valide
-     *
-     * @param boolean $valide
-     *
-     * @return Element
-     */
-    public function setValide($valide)
-    {
-        $this->valide = $valide;
-
-        return $this;
-    }
-
-    /**
-     * Get valide
-     *
-     * @return boolean
-     */
-    public function getValide()
-    {
-        return $this->valide;
-    }
-
-
-    /**
-     * Add product
-     *
-     * @param \Biopen\GeoDirectoryBundle\Document\ElementProduct $product
-     *
-     * @return Element
-     */
-    public function addProduct(\Biopen\GeoDirectoryBundle\Document\ElementProduct $product)
-    {
-        $this->products[] = $product;
-
-        return $this;
-    }
-
-    /**
-     * Remove product
-     *
-     * @param \Biopen\GeoDirectoryBundle\Document\ElementProduct $product
-     */
-    public function removeProduct(\Biopen\GeoDirectoryBundle\Document\ElementProduct $product)
-    {
-        $this->products->removeElement($product);
-    }
-
-    /**
-     * Get products
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getProducts()
-    {
-        return $this->products;
-    }
-
-    /**
      * Set lat
      *
-     * @param int $lat
+     * @param float $lat
      * @return $this
      */
     public function setLat($lat)
@@ -543,7 +212,7 @@ class Element
     /**
      * Get lat
      *
-     * @return int $lat
+     * @return float $lat
      */
     public function getLat()
     {
@@ -553,7 +222,7 @@ class Element
     /**
      * Set lng
      *
-     * @param int $lng
+     * @param float $lng
      * @return $this
      */
     public function setLng($lng)
@@ -565,10 +234,260 @@ class Element
     /**
      * Get lng
      *
-     * @return int $lng
+     * @return float $lng
      */
     public function getLng()
     {
         return $this->lng;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     * @return $this
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string $adresse
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string $description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set tel
+     *
+     * @param string $tel
+     * @return $this
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+        return $this;
+    }
+
+    /**
+     * Get tel
+     *
+     * @return string $tel
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     * @return $this
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string $mail
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set webSite
+     *
+     * @param string $webSite
+     * @return $this
+     */
+    public function setWebSite($webSite)
+    {
+        $this->webSite = $webSite;
+        return $this;
+    }
+
+    /**
+     * Get webSite
+     *
+     * @return string $webSite
+     */
+    public function getWebSite()
+    {
+        return $this->webSite;
+    }
+
+    /**
+     * Set mainProduct
+     *
+     * @param string $mainProduct
+     * @return $this
+     */
+    public function setMainProduct($mainProduct)
+    {
+        $this->mainProduct = $mainProduct;
+        return $this;
+    }
+
+    /**
+     * Get mainProduct
+     *
+     * @return string $mainProduct
+     */
+    public function getMainProduct()
+    {
+        return $this->mainProduct;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string $type
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set contributor
+     *
+     * @param string $contributor
+     * @return $this
+     */
+    public function setContributor($contributor)
+    {
+        $this->contributor = $contributor;
+        return $this;
+    }
+
+    /**
+     * Get contributor
+     *
+     * @return string $contributor
+     */
+    public function getContributor()
+    {
+        return $this->contributor;
+    }
+
+    /**
+     * Set contributorMail
+     *
+     * @param string $contributorMail
+     * @return $this
+     */
+    public function setContributorMail($contributorMail)
+    {
+        $this->contributorMail = $contributorMail;
+        return $this;
+    }
+
+    /**
+     * Get contributorMail
+     *
+     * @return string $contributorMail
+     */
+    public function getContributorMail()
+    {
+        return $this->contributorMail;
+    }
+
+    /**
+     * Set validationCode
+     *
+     * @param string $validationCode
+     * @return $this
+     */
+    public function setValidationCode($validationCode)
+    {
+        $this->validationCode = $validationCode;
+        return $this;
+    }
+
+    /**
+     * Get validationCode
+     *
+     * @return string $validationCode
+     */
+    public function getValidationCode()
+    {
+        return $this->validationCode;
+    }
+
+    /**
+     * Add product
+     *
+     * @param Biopen\GeoDirectoryBundle\Document\ElementProduct $product
+     */
+    public function addProduct(\Biopen\GeoDirectoryBundle\Document\ElementProduct $product)
+    {
+        $this->products[] = $product;
+    }
+
+    /**
+     * Remove product
+     *
+     * @param Biopen\GeoDirectoryBundle\Document\ElementProduct $product
+     */
+    public function removeProduct(\Biopen\GeoDirectoryBundle\Document\ElementProduct $product)
+    {
+        $this->products->removeElement($product);
+    }
+
+    /**
+     * Get products
+     *
+     * @return \Doctrine\Common\Collections\Collection $products
+     */
+    public function getProducts()
+    {
+        return $this->products;
     }
 }
