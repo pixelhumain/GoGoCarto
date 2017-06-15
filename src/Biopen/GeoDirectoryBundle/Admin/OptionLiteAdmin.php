a61536325f60d190bac29cb675f88f3fdb48fc69
Sebastian Castro <90scastro@gmail.com>
2017-06-15 22:43:39 +0200

    Cleans and add link action

--- src/Biopen/GeoDirectoryBundle/Admin/OptionLiteAdmin.php
+++ src/Biopen/GeoDirectoryBundle/Admin/OptionLiteAdmin.php
@@ -3,7 +3,7 @@
  * @Author: Sebastian Castro
  * @Date:   2017-03-28 15:29:03
  * @Last Modified by:   Sebastian Castro
- * @Last Modified time: 2017-05-30 18:49:22
+ * @Last Modified time: 2017-06-15 22:32:18
  */
 namespace Biopen\GeoDirectoryBundle\Admin;
 
@@ -23,40 +23,17 @@ class OptionLiteAdmin extends AbstractAdmin
    protected $baseRouteName = 'admin_biopen_geodirectory_option_lite';
 	protected $baseRoutePattern = 'admin_biopen_geodirectory_option_lite';
 
-   public function createQuery($context = 'list')
-	{
-	    $query = parent::createQuery($context);
-	    return $query;
-	}
-
 	protected function configureFormFields(FormMapper $formMapper)
 	{
 	  $formMapper
-		  	->add('name', null, array('required' => true, 'label' => 'Nom'))
-		  	->add('nameShort', null, array('required' => false, 'label' => 'Nom court'))
+		  	->add('name', null, array('required' => true,'label' => 'Nom'))		  	
 		  	->add('index', null, array('required' => true, 'label' => 'Position'))
 		  	->add('color', 'xmon_color_picker', array('required' => false, 'label' => 'Couleur'))		
 		  	->add('softColor', 'xmon_color_picker', array('required' => false, 'label' => 'Couleur adoucie'))		  	
-		  	->add('icon', null, array('required' => false, 'label' => 'Icone'))			  	
+		  	->add('icon', null, array('required' => false, 'label' => 'Icone'))
 		  	->add('useIconForMarker', null, array('required' => false, 'label' => "Icone  pour le marqueur"))		
 		  	->add('useColorForMarker', null, array('required' => false, 'label' => "Couleur pour le marqueur"))				  
-			->add('displayOption', null, array('required' => false, 'label' => "Activer l'option"))	 ;   
-	}
-
-	protected function configureListFields(ListMapper $listMapper)
-	{
-	  $listMapper
-	      ->addIdentifier('name')	 
-	      ->add('_action', 'actions', array(
-                'actions' => array(
-                    'edit' => array(),
-                    'delete' => array()
-                )
-            ));   
-	}
-
-	protected function configureRoutes(RouteCollection $collection)
-	{
-	    $collection->add('move', $this->getRouterIdParameter().'/move/{position}');
+			->add('displayOption', null, array('required' => false, 'label' => "Activer l'option"))
+			->add('_link', 'text', array('required' => false, 'mapped' => false, 'label' => 'admin_biopen_geodirectory_option_edit')); 
 	}
 }
