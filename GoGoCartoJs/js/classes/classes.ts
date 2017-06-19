25264a9406258695fd92681dbc1f8d1da7196fc2
Sebastian Castro <90scastro@gmail.com>
2017-06-19 17:06:40 +0200

    Deletes files moved to GoGoCartoJs sub project

--- GoGoCartoJs/js/classes/classes.ts
+++
@@ -1,5 +0,0 @@
-export { Category } from "./category.class";
-export { Element } from "./element.class";
-export { Option } from "./option.class";
-export { OptionValue } from "./option-value.class";
-export { CategoryValue } from "./category-value.class";
