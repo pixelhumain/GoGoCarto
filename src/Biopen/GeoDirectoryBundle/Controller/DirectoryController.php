623c424468ac25ee3f061faa45935e3a88c64021
Sebastian Castro <90scastro@gmail.com>
2017-06-22 19:15:57 +0200

    Removes directory routes and actions, keep only the main one

--- src/Biopen/GeoDirectoryBundle/Controller/DirectoryController.php
+++ src/Biopen/GeoDirectoryBundle/Controller/DirectoryController.php
@@ -7,7 +7,7 @@
  *
  * @copyright Copyright (c) 2016 Sebastian Castro - 90scastro@gmail.com
  * @license    MIT License
- * @Last Modified time: 2017-06-19 10:55:42
+ * @Last Modified time: 2017-06-22 16:54:16
  */
  
 
@@ -32,143 +32,22 @@ class DirectoryController extends Controller
 {
     private $callnumber = 0;    
 
-    public function normalAction($mode, $addressAndViewport, Request $request)
+    public function renderAction(Request $request)
     {
-        list($address, $viewport) = $this->parseAddressViewport($addressAndViewport);   
-
-        $initialState['address'] = $address;
-        $initialState['viewport'] = $viewport;
-        $initialState['mode'] = $this->formatMode($mode);
-        $initialState['state'] = 'Normal';
-        $initialState['dataType'] = 'All';
-
-        return $this->renderDirectory($initialState, $request);
-    }  
-
-    public function showElementAction($name, $id, $addressAndViewport, Request $request)
-    {
-        list($address, $viewport) = $this->parseAddressViewport($addressAndViewport);
-
-        $initialState['address'] = $address;
-        $initialState['viewport'] = $viewport;
-        $initialState['mode'] = 'Map';
-        $initialState['state'] = 'ShowElementAlone';
-        $initialState['dataType'] = 'All';
-        $initialState['id'] = $id;
-
-        return $this->renderDirectory($initialState, $request);  
-    }   
-
-    public function showDirectionsAction($name, $id, $addressAndViewport, Request $request)
-    {
-        list($address, $viewport) = $this->parseAddressViewport($addressAndViewport);
-
-        $initialState['address'] = $address;
-        $initialState['viewport'] = $viewport;
-        $initialState['mode'] = 'Map';
-        $initialState['state'] = 'ShowDirections';
-        $initialState['dataType'] = 'All';
-        $initialState['id'] = $id;
-
-        return $this->renderDirectory($initialState, $request);  
-    }    
-
-    public function constellationAction($mode, $address, $range, Request $request)
-    {             
-        $address = $this->checkAddressInSession($address);
-
-        $initialState['address'] = $address;
-        $initialState['viewport'] = '';
-        $initialState['mode'] = $this->formatMode($mode);
-        $initialState['state'] = 'Constellation';
-        $initialState['range'] = $range;
-
-        return $this->renderDirectory($initialState, $request);    
-    } 
-
-    public function searchAction($mode, $text, Request $request)
-    {
-        $initialState['mode'] = $this->formatMode($mode);
-        $initialState['state'] = 'Normal';
-        $initialState['dataType'] = 'SearchResults';
-        $initialState['text'] = $text;
-
-        return $this->renderDirectory($initialState, $request);
-    }  
-
-    private function renderDirectory($initialState, Request $request)
-    {
-        $initialState['filters'] = $request->query->get('cat');        
-
-        $serializer = $this->container->get('jms_serializer');
-
-        $em = $this->get('doctrine_mongodb')->getManager();
-
-        // will be deleted when creating template from javascvript
-        $taxonomy = $em->getRepository('BiopenGeoDirectoryBundle:Taxonomy')->findAll()[0];
-        $mainCategory = $taxonomy->getMainCategory();
-        $openHours = $taxonomy->getOpenHoursCategory();
-      
-        $optionsList = $em->getRepository('BiopenGeoDirectoryBundle:Option')->findAll(); 
+        $em = $this->get('doctrine_mongodb')->getManager();      
 
         $taxonomyRep = $em->getRepository('BiopenGeoDirectoryBundle:Taxonomy');
 
         $mainCategoryJson = $taxonomyRep->findMainCategoryJson();
         $openHoursCategoryJson = $taxonomyRep->findOpenHoursCategoryJson();
 
-        if (!$mainCategoryJson) $mainCategoryJson = $serializer->serialize($mainCategory, 'json'); 
-        if (!$openHoursCategoryJson) $openHoursCategoryJson = $serializer->serialize($openHours, 'json'); 
-
         $securityContext = $this->container->get('security.context');
         $isAdmin = $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED') && $securityContext->getToken()->getUser()->isAdmin();
         $isAdmin = $isAdmin ? 'true' : 'false';
         
         return $this->render('BiopenGeoDirectoryBundle:directory:directory.html.twig', 
-                              array("mainCategory" => $mainCategory, "openHoursCategory" => $openHours,
-                                    "initialState" => $initialState, "optionList" => $optionsList,
-                                    "mainCategoryJson" => $mainCategoryJson, 'openHoursCategoryJson' => $openHoursCategoryJson,
+                              array("mainCategoryJson" => $mainCategoryJson, 'openHoursCategoryJson' => $openHoursCategoryJson,
                                     'isAdmin' => $isAdmin));
-    }
-
-    private function getMainCategory()
-    {
-        $em = $this->get('doctrine_mongodb')->getManager();
-
-        // Get Product List        
-        $mainCategory = $em->getRepository('BiopenGeoDirectoryBundle:Category')
-        ->findOneByDepth(0); 
-
-        return $mainCategory;
-    }
-
-    private function getOpenHoursCategory()
-    {
-        $em = $this->get('doctrine_mongodb')->getManager();
-
-        // Get OpenHours list, created with depth -1        
-        $openHoursCategory = $em->getRepository('BiopenGeoDirectoryBundle:Category')
-        ->findOneByDepth(-1); 
-
-        return $openHoursCategory;
-    }
-
-    private function parseAddressViewport($addressViewport)
-    {
-        $splited = explode('@', $addressViewport);
-
-        if (count($splited) == 1)
-        {
-            return array($addressViewport, '');
-        }
-        else
-        {
-            return $splited;
-        }  
-    }
-
-    private function formatMode($mode)
-    {
-        return $mode == 'carte' ? 'Map' : 'List';
     }
   
 }
