d664dc2256931448d8d045c99b9ba5961de911d6
Sebastian Castro <90scastro@gmail.com>
2017-06-29 10:35:10 +0200

    Change geocoder to google map

--- src/Biopen/GeoDirectoryBundle/Resources/js/element-form/init-map.js
+++ src/Biopen/GeoDirectoryBundle/Resources/js/element-form/init-map.js
@@ -5,7 +5,7 @@
  *
  * @copyright Copyright (c) 2016 Sebastian Castro - 90scastro@gmail.com
  * @license    MIT License
- * @Last Modified time: 2017-05-05 08:40:48
+ * @Last Modified time: 2017-06-29 10:25:15
  */
 var map;
 var geocoder;
@@ -44,7 +44,7 @@ function initMap()
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2ViYWxsb3QiLCJhIjoiY2l4MGtneGVjMDF0aDJ6cWNtdWFvc2Y3YSJ9.nIZr6G2t08etMzft_BHHUQ').addTo(map);
 	
-	geocoder = GeocoderJS.createGeocoder('openstreetmap');
+	geocoder = GeocoderJS.createGeocoder({'provider': 'google'});
 	
 	if (markerPosition) createMarker(markerPosition);
 }
