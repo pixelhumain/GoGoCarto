985db0a32bb2873d2eb95c1a568324f4913d20eb
Sebastian Castro <90scastro@gmail.com>
2017-07-11 12:31:11 +0200

    Revert "Get database name with the url (HTTP_HOST)"
    This reverts commit e798ce9468830721b3b0ce2b4762e39ae90d8db7.

--- app/config/config_database_name.php
+++
@@ -1,3 +0,0 @@
-<?php
-// get the database name from the url. For exemple my-project.gogocarto.fr will use the database name "my-project"
-$container->setParameter('database_name', explode('.', $_SERVER["HTTP_HOST"])[0]);
