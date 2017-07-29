2d70c2960cffcb10160049e5bb63a8ed7a497587
Sebastian Castro <90scastro@gmail.com>
2017-07-29 18:08:44 +0200

    Change wrappers and baseline fixtures

--- src/Biopen/CoreBundle/DataFixtures/MongoDB/loadWrapper.php
+++ src/Biopen/CoreBundle/DataFixtures/MongoDB/loadWrapper.php
@@ -14,22 +14,22 @@ class LoadWrapper implements FixtureInterface
   public function load(ObjectManager $manager)
   {  
     $new_wrapper = new Wrapper();
-    $new_wrapper->setTitle('Du bio, mais pas que !'); 
-    $new_wrapper->setContent("Pas besoin d'avoir le label Bio pour être inscrit ! L'accent est mis sur le local, et sur une agriculture sans pesticide de préférence à petite échelle.");
+    $new_wrapper->setTitle('Du bio, du local, du solidaire !'); 
+    $new_wrapper->setContent("Produits bio, équitables et locaux, amap, éco-artisans, mode éthique, écoles alternatives… Découvrez nos 12 000 bons plans !");
     $new_wrapper->setBackgroundColor('ffffff');
     $new_wrapper->setTextColor("inherit");
 
     $manager->persist($new_wrapper);
     $new_wrapper = new Wrapper();
-    $new_wrapper->setTitle('Open Source'); 
-    $new_wrapper->setRawContent("<span style=\"text-transform: uppercase;font-weight: bold;padding-right: 5px;color: #bd2d86;\">Près de chez nous</span> est un site gratuit et libre de droits. Il a été développé bénévolement en tant que service d'utilité publique, par la communauté informatique Biopen. Le code est en accès libre sur <a href=\"https://github.com/Biopen/GoGoCarto\" style=\"font-weight:bold;color: #bdc900;\" target=\"_blank\">Github</a>");
+    $new_wrapper->setTitle("Ici, c'est open bar !"); 
+    $new_wrapper->setRawContent("<span style=\"text-transform: uppercase;font-weight: bold;padding-right: 5px;color: #bd2d86;\">Près de chez nous</span> est un site gratuit et libre de droits. Ça veut dire que le code source est en accès libre sur <a href=\"https://github.com/Biopen/GoGoCarto\" style=\"font-weight:bold;color: #bdc900;\" target=\"_blank\">Github</a>, à condition que vous le partagiez à votre tour :-)");
     $new_wrapper->setBackgroundColor('ffffff');
     $new_wrapper->setTextColor("inherit");
 
     $manager->persist($new_wrapper); 
     $new_wrapper = new Wrapper();
-    $new_wrapper->setTitle('En cours de développement'); 
-    $new_wrapper->setContent("Le site est encore en cours de développement. La base de donnée des elements est pour le moment fictive, et certains bugs peuvent encore faire leur apparition... Merci de votre compréhension !");
+    $new_wrapper->setTitle('Projet collaboratif'); 
+    $new_wrapper->setContent("Nous comptons sur la force du collectif pour mener à bien ce projet de référencement. Ainsi chaque citoyen est libre de contirbuer à cette carte, nous sommes également ouverts à des partenariats pour mettre en communs nos bases de données !");
     $new_wrapper->setBackgroundColor('ffffff');
     $new_wrapper->setTextColor("inherit");
 
