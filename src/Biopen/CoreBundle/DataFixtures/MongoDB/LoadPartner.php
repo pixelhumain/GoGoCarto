771e952355ef9a92fc2f38a7e58f3d2446d7c7e5
Sebastian Castro <90scastro@gmail.com>
2017-07-29 09:10:12 +0200

    Configure partners style

--- src/Biopen/CoreBundle/DataFixtures/MongoDB/LoadPartner.php
+++ src/Biopen/CoreBundle/DataFixtures/MongoDB/LoadPartner.php
@@ -15,18 +15,33 @@ class LoadPartner implements FixtureInterface
   {  
     $lipsum = new LoremIpsum();  
 
-    $names = array('colibris','Le marché citoyen','supaéro','Alternatiba');
-    foreach ($names as $key => $name) 
-    {
-      $new_partner = new Partner();
+    // $names = array('colibris','Le marché citoyen','supaéro','Alternatiba');
+    // foreach ($names as $key => $name) 
+    // {
+    //   $new_partner = new Partner();
 
-      $new_partner->setName($name); 
-      $new_partner->setContent($lipsum->words(rand(30,100)));
-      $new_partner->setLogoUrl("http://lorempixel.com/300/300/abstract/" . $key);
-      $new_partner->setWebsiteUrl("www.partenaire.com");
-      $manager->persist($new_partner);
-    }  
-     
+    //   $new_partner->setName($name); 
+    //   $new_partner->setContent($lipsum->words(rand(30,100)));
+    //   $new_partner->setLogoUrl("http://lorempixel.com/300/300/abstract/" . $key);
+    //   $new_partner->setWebsiteUrl("www.partenaire.com");
+    //   $manager->persist($new_partner);
+    // }  
+
+    $new_partner = new Partner();
+
+    $new_partner->setName("Colibris"); 
+    $new_partner->setContent($lipsum->words(rand(30,100)));
+    $new_partner->setLogoUrl("colibris.png");
+    $new_partner->setWebsiteUrl("www.mouvement-colibris.com");
+    $manager->persist($new_partner);
+
+    $new_partner = new Partner();
+
+    $new_partner->setName("Le Mlarché Citoyen"); 
+    $new_partner->setContent($lipsum->words(rand(30,100)));
+    $new_partner->setLogoUrl("marchecitoyen.png");
+    $new_partner->setWebsiteUrl("www.machercitoyen.net");
+    $manager->persist($new_partner);     
 
     // we trigger saving of all partners
     $manager->flush();
