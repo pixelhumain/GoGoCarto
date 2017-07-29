f3cf7b766a74c4c5c97d8efc61752eaedf02c37c
Sebastian Castro <90scastro@gmail.com>
2017-07-29 16:44:59 +0200

    Fixs logout bug when only email provided

--- src/Biopen/GeoDirectoryBundle/Resources/js/element-form/contributor-login.js
+++ src/Biopen/GeoDirectoryBundle/Resources/js/element-form/contributor-login.js
@@ -2,11 +2,16 @@
 * @Author: Sebastian Castro
 * @Date:   2017-04-16 09:32:02
 * @Last Modified by:   Sebastian Castro
-* @Last Modified time: 2017-04-30 12:15:45
+* @Last Modified time: 2017-07-29 16:38:31
 */
 
 jQuery(document).ready(function()
 {	
+	if (window.location.search.includes('logout') )
+	{
+		window.location.href = window.location.origin + window.location.pathname;
+	}	
+
 	$('#btn-login').on( "login", function() {
   	window.location.reload();
 	});
@@ -48,4 +53,22 @@ function checkLoginAndSend()
 
 		$('form[name="user"]').submit();		
 	}
+}
+
+function removeParam(key, sourceURL) {
+    var rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        rtn = rtn + "?" + params_arr.join("&");
+    }
+    return rtn;
 }
