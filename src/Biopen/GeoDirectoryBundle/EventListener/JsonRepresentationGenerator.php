3097637b63ee62c4aea62306e54f4f0b7260839f
Sebastian Castro <90scastro@gmail.com>
2017-10-05 08:31:56 +0200

    Cleans unused and comments

--- src/Biopen/GeoDirectoryBundle/EventListener/JsonRepresentationGenerator.php
+++ src/Biopen/GeoDirectoryBundle/EventListener/JsonRepresentationGenerator.php
@@ -3,7 +3,7 @@
  * @Author: Sebastian Castro
  * @Date:   2017-06-18 21:03:01
  * @Last Modified by:   Sebastian Castro
- * @Last Modified time: 2017-06-19 11:43:37
+ * @Last Modified time: 2017-10-04 14:50:25
  */
 namespace Biopen\GeoDirectoryBundle\EventListener;
 
@@ -31,19 +31,14 @@ class JsonRepresentationGenerator
 		$document = $args->getDocument();
 		$dm = $args->getDocumentManager();
 
-		if ($document instanceof Element) 
+		if ($document instanceof Option || $document instanceof Category)
 		{
-			//$this->updateElement($document);
-			return;
-	  }
-	  else if ($document instanceof Option || $document instanceof Category)
-	  {
-	  	  if (!$document->getIsFixture()) $this->updateTaxonomy($dm);
-	  }
-	  else if ($document instanceof Taxonomy)
-	  {
-	  	  $this->updateTaxonomy($dm);
-	  }
+			if (!$document->getIsFixture()) $this->updateTaxonomy($dm);
+		}
+		else if ($document instanceof Taxonomy)
+		{
+			$this->updateTaxonomy($dm);
+		}
 	}
 
 	public function postUpdate(\Doctrine\ODM\MongoDB\Event\LifecycleEventArgs $args)
@@ -51,19 +46,14 @@ class JsonRepresentationGenerator
 		$document = $args->getDocument();
 		$dm = $args->getDocumentManager();
 
-		if ($document instanceof Element) 
+		if ($document instanceof Option || $document instanceof Category)
 		{
-			// $this->updateElement($document);
-			return;
-	  }
-	  else if ($document instanceof Option || $document instanceof Category)
-	  {
-	  	  $this->updateTaxonomy($dm);
-	  }
-	  else if ($document instanceof Taxonomy)
-	  {
-	  	  $this->updateTaxonomy($dm);
-	  }
+			$this->updateTaxonomy($dm);
+		}
+		else if ($document instanceof Taxonomy)
+		{
+			$this->updateTaxonomy($dm);
+		}
 	}
 
 	public function postRemove(\Doctrine\ODM\MongoDB\Event\LifecycleEventArgs $args)
@@ -97,36 +87,4 @@ class JsonRepresentationGenerator
 		$dm->persist($taxonomy);
 		$dm->flush();
 	}
-
-	// private function updateElement($element)
-	// {
-	// 	if (!$element->getOptionValues() || !$element->getCoordinates()) { $element->setName("pas optionvalues ou coordinnes"); return; }
-
-	// 	$fullJson = json_encode($element);
-	// 	$fullJson = rtrim($fullJson,'}');
-	// 	$fullJson .= ', "optionValues": [';
-
-	// 	foreach ($element->getOptionValues() as $key => $value) {
-	// 	   $fullJson .= '{ "optionId" :'.$value->getOptionId().', "index" :'.$value->getIndex();
-	// 	   if ($value->getDescription()) $fullJson .=  ', "description" : "' . $value->getDescription() . '"';
-	// 	   $fullJson .= '}';
-	// 	   if ($key != count($element->getOptionValues()) -1) $fullJson .= ',';
-	// 	}
-
-	// 	$fullJson .= ']';
-	// 	if ($element->getModifiedElement()) $fullJson .= ', "modifiedElement": ' . $element->getModifiedElement()->getFullJson();
-	// 	$fullJson .= '}';
-
-	// 	$element->setFullJson($fullJson);  
-
-	// 	$compactJson = '["'.$element->getId() . '",' .$element->getStatus() . ',"' .$element->getName() . '",'. $element->getCoordinates()->getLat() .','. $element->getCoordinates()->getLng().', [';
-	// 	foreach ($element->getOptionValues() as $key => $value) {
-	// 	   $compactJson .= '['.$value->getOptionId().','.$value->getIndex();
-	// 	   //if ($value->getDescription()) $responseJson .=  ',' . $value->getDescription();
-	// 	   $compactJson .= ']';
-	// 	   if ($key != count($element->getOptionValues()) -1) $compactJson .= ',';
-	// 	}
-	// 	$compactJson .= ']]';
-	// 	$element->setCompactJson($compactJson);
-	// }
 }
