a9a3d5476fc72992f1d1888f6c1577e009e1cdab
Sebastian Castro <90scastro@gmail.com>
2017-10-31 18:30:03 +0100

    Change cache clearing process

--- cache_clear.sh
+++ cache_clear.sh
@@ -1,4 +1,10 @@
 #!/bash/bin
-php bin/console cache:clear
-php bin/console cache:clear --env=prod
-sudo setfacl -R -m u:"www-data":rwX -m u:`whoami`:rwX var/cache var/logs
+# php bin/console cache:clear
+# php bin/console cache:clear --env=prod
+# sudo setfacl -R -m u:"www-data":rwX -m u:`whoami`:rwX var/cache var/logs
+
+systemctl stop mongod
+rm -R var/cache/*
+chmod 777 -R var/cache/
+systemctl start mongod
+
