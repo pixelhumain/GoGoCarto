abc0606444caa49c1cd2f563997730b3f345e48e
Sebastian Castro <90scastro@gmail.com>
2017-11-20 09:13:26 +0100

    Adds descriptionMore to form

--- src/Biopen/GeoDirectoryBundle/Form/ElementType.php
+++ src/Biopen/GeoDirectoryBundle/Form/ElementType.php
@@ -7,7 +7,7 @@
  *
  * @copyright Copyright (c) 2016 Sebastian Castro - 90scastro@gmail.com
  * @license    MIT License
- * @Last Modified time: 2017-11-08 11:04:18
+ * @Last Modified time: 2017-11-19 18:24:25
  */
  
 
@@ -58,6 +58,7 @@ class ElementType extends AbstractType
           ->add('name', TextType::class, array('required' => false))
           ->add('address', TextType::class, array('required' => false))
           ->add('description', TextareaType::class, array('required' => false))
+          ->add('descriptionMore', TextareaType::class, array('required' => false))
           ->add('commitment', TextareaType::class, array('required' => false))
           ->add('tel', TextType::class, array('required' => false)) 
           ->add('webSite', TextType::class, array('required' => false)) 
