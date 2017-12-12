9dd2387e337e48985dac2220bde43b11f2c1337e
Sebastian Castro <90scastro@gmail.com>
2017-12-12 15:34:11 +0100

    Adds special action delete element reported as no exist

--- src/Biopen/GeoDirectoryBundle/Controller/Admin/SpecialActionsController.php
+++ src/Biopen/GeoDirectoryBundle/Controller/Admin/SpecialActionsController.php
@@ -120,8 +120,7 @@ class SpecialActionsController extends Controller
     public function verifyDuplicates($element)
     {
         if ($element->getStatus() == ElementStatus::Duplicate)
-        {
-            
+        {            
             $em = $this->get('doctrine_mongodb')->getManager();
 
             $em = $this->get('doctrine_mongodb')->getManager();
@@ -166,6 +165,37 @@ class SpecialActionsController extends Controller
                 dump($elements);
             }
         }
+    }
+
+    public function deleteElementReportedAsNoMoreExistingAction()
+    {
+        $em = $this->get('doctrine_mongodb')->getManager();
+        $repo = $em->getRepository('BiopenGeoDirectoryBundle:Element');   
+        $elements = $repo->findModerationNeeded(false, 1);
+        dump(count($elements));
+
+        $actionService = $this->get('biopen.element_action_service');
+
+        $i = 0;
+        foreach($elements as $key => $element) 
+        {
+           $unresolvedReports = $element->getUnresolvedReports();
+           $noExistReports = array_filter($unresolvedReports , function($r) { return $r->getValue() == 0; });
+           if (count($noExistReports) > 0 && count($noExistReports) == count($unresolvedReports)) {
+             $actionService->delete($element);
+             dump($element->getName());
+           }
+            if ((++$i % 20) == 0) {
+                $em->flush();
+                $em->clear();
+            }
+        }
+
+        $em->flush();
+        $em->clear(); 
+
+        return new Response("Les éléments ont été mis à jours avec succès.");
+        //$em->flush();
     }
 
     public function addImportContributionAction()
