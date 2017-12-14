bd041c6ff73f49288f8edb60c6b76fe858c80df5
Sebastian Castro <90scastro@gmail.com>
2017-12-14 12:39:04 +0100

    Fixs contributor login mail error message

--- src/Biopen/GeoDirectoryBundle/Resources/js/element-form/contributor-login.js
+++ src/Biopen/GeoDirectoryBundle/Resources/js/element-form/contributor-login.js
@@ -2,7 +2,7 @@
 * @Author: Sebastian Castro
 * @Date:   2017-04-16 09:32:02
 * @Last Modified by:   Sebastian Castro
-* @Last Modified time: 2017-08-04 12:15:52
+* @Last Modified time: 2017-12-14 12:38:22
 */
 
 jQuery(document).ready(function()
@@ -25,15 +25,8 @@ function checkLoginAndSend()
 {
 	$('.required').each(function ()
 	{ 
-		if(!$(this).val()) 
-		{
-			$(this).addClass('invalid');			
-		}
-		else
-		{
-			$(this).removeClass('invalid');
-			$(this).closest('.input-field').removeClass('error');
-		}
+		if(!$(this).val()) $(this).addClass('invalid');		
+		else $(this).closest('.input-field').removeClass('error');
 	});
 	
 	$('.invalid').each(function ()
@@ -53,22 +46,4 @@ function checkLoginAndSend()
 
 		$('form[name="user"]').submit();		
 	}
-}
-
-function removeParam(key, sourceURL) {
-    var rtn = sourceURL.split("?")[0],
-        param,
-        params_arr = [],
-        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
-    if (queryString !== "") {
-        params_arr = queryString.split("&");
-        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
-            param = params_arr[i].split("=")[0];
-            if (param === key) {
-                params_arr.splice(i, 1);
-            }
-        }
-        rtn = rtn + "?" + params_arr.join("&");
-    }
-    return rtn;
 }
